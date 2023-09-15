1c55b37ac3b7e8d84554ef44c4c5f091945ed3f6
wonedd <vinmarv@hotmail.com>
2023-09-15 17:47:12 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import { Elysia } from "elysia";
+// server.ts
+import { Elysia, t } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { PrismaClient } from '@prisma/client'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const setup = (app: Elysia) => app.decorate('db', new PrismaClient())
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+    .use(setup)
+    .use(swagger({
+        documentation:{
+            info:{
+                title:"Chase api doc",
+                version:"0.0.1",
+                description:"Api piloto do chase para usando bun.js"
+            }
+        }
+    }))
+    .get('/search', ({ db }) => db.chaseio.findMany({ take:10000 }))
+    .listen(8080)
+
+export type App = typeof app
